232f4a2bd0f3628f17952a786d43458bd3d99bd1
Tadeo-sosa <tadeofersosa15@gmail.com>
2026-04-14 10:57:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-const Valkey = require("iovalkey");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const valkey = new Valkey();
-
-const KEY = "comentarios";
+let comentarios = []; // guardamos en memoria
 
 // CREATE
-app.post("/comentarios", async (req, res) => {
-    await valkey.rpush(KEY, req.body.texto);
+app.post("/comentarios", (req, res) => {
+    comentarios.push(req.body.texto);
     res.send("ok");
 });
 
 // READ
-app.get("/comentarios", async (req, res) => {
-    let datos = await valkey.lrange(KEY, 0, -1);
-    res.json(datos);
+app.get("/comentarios", (req, res) => {
+    res.json(comentarios);
 });
 
 // DELETE
-app.delete("/comentarios/:id", async (req, res) => {
-    let datos = await valkey.lrange(KEY, 0, -1);
-    datos.splice(req.params.id, 1);
-
-    await valkey.del(KEY);
-
-    if (datos.length > 0) {
-        await valkey.rpush(KEY, ...datos);
-    }
-
+app.delete("/comentarios/:id", (req, res) => {
+    comentarios.splice(req.params.id, 1);
     res.send("ok");
 });
 
 // UPDATE
-app.put("/comentarios/:id", async (req, res) => {
-    let datos = await valkey.lrange(KEY, 0, -1);
-
-    datos[req.params.id] = req.body.texto;
-
-    await valkey.del(KEY);
-
-    if (datos.length > 0) {
-        await valkey.rpush(KEY, ...datos);
-    }
-
+app.put("/comentarios/:id", (req, res) => {
+    comentarios[req.params.id] = req.body.texto;
     res.send("ok");
 });
 
-// SIEMPRE AL FINAL
-app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+// IMPORTANTE
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
